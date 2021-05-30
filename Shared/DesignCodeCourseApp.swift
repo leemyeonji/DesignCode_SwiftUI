80805683e26c46f4ad9b68677f83ca04f9ecd5cc
leemyeonji <61834038+leemyeonji@users.noreply.github.com>
2021-05-30 14:29:33 +0900

    Initial setting

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by 임현지 on 2021/05/30.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
