699dc90f320b192ad912b891e2e5d9bff6192973
leemyeonji <61834038+leemyeonji@users.noreply.github.com>
2021-05-30 22:53:44 +0900

    LazyVGrid couse list

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -16,11 +16,13 @@ struct CoursesView: View {
     var body: some View {
         ZStack {
             ScrollView {
-                VStack(spacing: 20) {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 160), spacing: 16)],
+                          spacing : 16
+                ) {
                     ForEach(courses) { item in
                         CourseItem(course: item)
                             .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
-                            .frame(width: 335, height: 250)
+                            .frame(height: 200)
                             .onTapGesture {
                                 withAnimation(.spring()) {
                                     show.toggle()
@@ -31,6 +33,7 @@ struct CoursesView: View {
                             .disabled(isDisabled)
                     }
                 }
+                .padding(16)
                 .frame(maxWidth: .infinity)
             }
             
